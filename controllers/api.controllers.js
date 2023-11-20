1b2babd5f6969937c01edb3663b5ee1f6ad49947
MHWatchy <mwatchy@gmail.com>
2023-11-20 12:26:11 +0000

    Removed unnecessary code

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,8 +1,5 @@
-const { returnEndpoints } = require("../models/api.models")
 const endpoints = require("../endpoints.json")
 
-exports.getApi = (req, res, err) => {
-    returnEndpoints().then(() => {
-        res.status(200).send({endpoints})
-    })
+exports.getApi = (req, res, next) => {
+  res.status(200).send({ endpoints })
 }
