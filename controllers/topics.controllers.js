2eb6c5327d918b710dc195565e4128713207e5d2
MHWatchy <mwatchy@gmail.com>
2023-11-23 09:55:11 +0000

    Light refactoring

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,7 +1,6 @@
 const { selectAllTopics } = require("../models/topics.models")
 
 exports.getTopics = (req, res, next) => {
-  console.log("Get All Topics")
   selectAllTopics().then((topics) => {
     res.status(200).send({ topics })
   }).catch(next)
